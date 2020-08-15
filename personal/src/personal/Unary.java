89e7fecefe1792541194c2b8560f1e5eb900d4e5
kanggusdn <gusdnkang.DE@gmai.com>
2020-08-15 21:26:33 +0900

    저장

---
+++ personal/src/personal/Unary.java
@@ -0,0 +1,15 @@
+package personal;
+
+public class Unary {
+
+	public static void main(String[] args) {
+        int a = 20, b = 20;
+		
+		int c = 400, d = 500, e = 600;
+		
+		System.out.println("a++ = "+ a++ + ", ++b = " + (++b));
+		System.out.println("a = " + a + ", b = " +b);
+
+	}
+
+}
