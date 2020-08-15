ee8607678e099bb110249842fc82414c30d95475
kanggusdn <gusdnkang.DE@gmail.com>
2020-08-15 22:08:49 +0900

    Unary

--- doit/src/doit/Unary.java
+++ doit/src/doit/Unary.java
@@ -3,10 +3,7 @@ package doit;
 public class Unary {
 
 	public static void main(String[] args) {
-		int a = 10, b = 20;
-		
-		System.out.println("a++ = "+ a++ + ", ++b = " + (++b));
-		System.out.println("a = " + a + ", b = " +b);
+		// TODO Auto-generated method stub
 
 	}
 
