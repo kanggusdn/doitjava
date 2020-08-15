41007b25cdcd4dbb44a1aec5f20692d029184c2e
kanggusdn <gusdnkang.DE@gmail.com>
2020-08-16 07:20:18 +0900

    개인 연습용 Arithmetic

---
+++ doit/src/doit/Arithmetic.java
@@ -0,0 +1,27 @@
+package doit;
+
+public class Arithmetic {
+
+	public static void main(String[] args) {
+		int iRes; //intResult
+		double dRes; //doubleResult
+
+		iRes = 6+4;
+		System.out.println(iRes);
+		iRes = 6-4;
+		System.out.println(iRes);
+		iRes = 6*4;
+		System.out.println(iRes);
+		iRes = 6/4;
+		System.out.println(iRes);
+		dRes = 6.0/4.0;
+		System.out.println(dRes);
+		iRes = 6%4;
+		System.out.println(iRes);
+
+		dRes = (double)6/4;
+		System.out.println(dRes);
+
+	}
+
+}
--- doit/src/doit/Unary.java
+++ doit/src/doit/Unary.java
@@ -3,7 +3,10 @@ package doit;
 public class Unary {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        int a = 10, b = 20;
+		
+		System.out.println("a++ = "+ a++ + ", ++b = " + (++b));
+		System.out.println("a = " + a + ", b = " +b);
 
 	}
 
