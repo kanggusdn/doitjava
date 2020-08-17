164276bd2911ad999bd8f75e3e6d064850529bae
kanggusdn <gusdnkang.DE@gmail.com>
2020-08-17 10:46:13 +0900

    char 연습용

---
+++ doit!/src/Char.java
@@ -0,0 +1,22 @@
+
+public class Char {
+
+	public static void main(String[] args) {
+		char ch1 = 'B';
+		System.out.println(ch1);
+//		
+		int num1 = ch1;
+		System.out.println(num1);
+		
+		char ch2 = 66;
+		char ch3 = 67;
+		System.out.println(ch2);
+		System.out.println(ch3);
+		
+		char ch4 = 'b';
+		int num4 = ch4;
+		System.out.println(num4);
+
+		System.out.println('b' - 'B');
+	}
+}
