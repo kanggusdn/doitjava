98dfd5b1defcf7cce5b0ca1ad1391311ca19bbd0
kanggusdn <gusdnkang.DE@gmail.com>
2020-08-18 15:37:11 +0900

    논리 연산자와 조건 연산자(feat. 복합 대입 연산자)

---
+++ chapter03/src/chapter03/AssingnmentEx.java
@@ -0,0 +1,18 @@
+package chapter03;
+
+public class AssingnmentEx {
+
+	public static void main(String[] args) {
+		int res = 0;
+		res += 10;
+		
+		System.out.println("res = " + res);
+		System.out.println(res -= 5);
+		System.out.println(res *= 3);
+		System.out.println(res /= 5);
+		System.out.println(res %= 3);
+		
+
+	}
+
+}
---
+++ chapter03/src/chapter03/OperationEx4.java
@@ -0,0 +1,20 @@
+package chapter03;
+
+public class OperationEx4 {
+
+	public static void main(String[] args) {
+		int fatherAge = 45;
+		int motherAge = 47;
+		
+		char ch;
+		ch = (fatherAge > motherAge) ? 'T' : 'F';
+			
+		System.out.println(ch);
+
+		int num = 10;
+		boolean isEven;
+		isEven = (num % 2) == 0 ? true : false;
+		System.out.println("짝수인가요? " + isEven);
+	}
+
+}
