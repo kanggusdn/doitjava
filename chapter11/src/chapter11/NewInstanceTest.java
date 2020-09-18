ce503831ab0400af970efd6aafc982df762009ef
kanggusdn <gusdnkang.DE@gmail.com>
2020-09-18 16:07:41 +0900

    <?> 관련 설명 추가

--- chapter11/src/chapter11/NewInstanceTest.java
+++ chapter11/src/chapter11/NewInstanceTest.java
@@ -5,7 +5,7 @@ public class NewInstanceTest {
 		Person person1 = new Person();
 		System.out.println(person1);
 		
-		Class pClass = Class.forName("chapter11.Person");
+		Class<?> pClass = Class.forName("chapter11.Person");	//<?> 모든 객체를 받아들이는 주문(?)
 		Person person2 = (Person)pClass.newInstance();
 		System.out.println(person2);
 	}
