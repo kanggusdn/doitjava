8f66dc8ed0b84286f7a571bf4b74cdb11b0d5c40
kanggusdn <gusdnkang.DE@gmail.com>
2020-09-25 16:03:20 +0900

    람다식, Consumer, Supplier, Function, Operator, Predicate 실행

---
+++ chapter13/src/chapter13/TestMyNumber.java
@@ -0,0 +1,33 @@
+package chapter13;
+
+public class TestMyNumber {
+	public static void main(String[] args) {
+//		MyNumber max = (x, y) -> (x >= y) ? x : y;
+//		System.out.println(max.getMax(10, 20));
+		
+		int num1 = 10;
+		int num2 = 20;
+		
+		//클래스 기반의 객체 생성
+		MyNumber in = new ImpleNumber();
+		int result = in.getMax(num1, num2);
+		System.out.println("in: " + result);
+		
+		//익명 구현 객체 생성
+		MyNumber in2 = new MyNumber() {
+			@Override
+			public int getMax(int num1, int num2) {
+				int result = num1 >= num2 ? num1 : num2;
+				return result;
+			}
+		};
+		result = in2.getMax(num1, num2);
+		System.out.println("in2: " + result);
+		
+		//람다식 사용
+		MyNumber in3 = (x, y) -> x >= y ? x : y;	//자료형 괄호 생략, 중괄호 생략, return 생략(한 줄일 경우)
+		result = in3.getMax(num1, num2);
+		System.out.println("in3: " + result);
+		
+	}
+}
