94d8eec712122af50a63394e8cd50a4f7bd4e58d
kanggusdn <gusdnkang.DE@gmail.com>
2020-09-30 08:39:36 +0900

    Reduction 복습

---
+++ Person/src/per/Student.java
@@ -0,0 +1,24 @@
+package per;
+
+public class Student implements Comparable<Student> {
+	private String name;
+	private int score;
+	
+	public Student (String name, int score) {
+		this.name = name;
+		this.score = score;
+	}
+	
+	public String getName() { return name; }
+	public int getScore() { return score; }
+	
+	@Override
+	public String toString() {
+		return name + "-" + score;
+	}
+	
+	@Override	//comparable 인터페이스의 추상 메소드
+	public int compareTo(Student o) {
+		return Integer.compare(this.score, o.score);	//매개값을 비교하여 -1, 0 , 1을 반환
+	}
+}
