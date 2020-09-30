95191d40db75008a51384f32de660232a4d8eeb1
kanggusdn <gusdnkang.DE@gmail.com>
2020-10-01 08:46:02 +0900

    FlatMap 복습

---
+++ Person/src/per/FlatMapExample.java
@@ -0,0 +1,21 @@
+package per;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class FlatMapExample {
+	public static void main(String[] args) {
+		List<String> inputList1 = Arrays.asList("java8 lambda", "stream mapping");
+		inputList1.stream().flatMap(data -> Arrays.stream(data.split(" "))).forEach(word -> System.out.println(word));
+		System.out.println();
+		List<String> inputList2 = Arrays.asList("10, 20, 30", "40, 50, 60");
+		inputList2.stream().flatMapToInt(data -> {
+			String[] strArr = data.split(",");
+			int[] intArr = new int[strArr.length];
+			for (int i = 0; i < strArr.length; i++) {
+				intArr[i] = Integer.parseInt(strArr[i].trim());	//trim: ÁÂ¿ì °ø¹é Á¦°Å / parseInt: ¹®ÀÚ¸¦ ¼ýÀÚ·Î ¹Ù²ãÁÜ
+			}
+			return Arrays.stream(intArr);
+		}).forEach(number -> System.out.println(number));
+	}
+}
