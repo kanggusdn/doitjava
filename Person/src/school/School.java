446ff1cc3ba9126377d17bb80508577a4825a67d
kanggusdn <gusdnkang.DE@gmail.com>
2020-10-06 22:22:57 +0900

    최종 프로젝트(오류 수정본)

---
+++ Person/src/school/School.java
@@ -0,0 +1,48 @@
+package school;
+
+import java.util.ArrayList;
+
+public class School {
+	private static School instance = new School();
+
+	private static String SCHOOL_NAME = "Good School";
+	private ArrayList<Student> studentList = new ArrayList<Student>();
+	private ArrayList<Subject> subjectList = new ArrayList<Subject>();
+
+	private School() {
+	}
+
+	public static School getInstance() {
+		if (instance == null)
+			instance = new School();
+		return instance;
+	}
+
+	public ArrayList<Student> getStudentList() {
+		return studentList;
+	}
+
+	public void addStudent(Student student) {
+		studentList.add(student);
+	}
+
+	public void addSubject(Subject subject) {
+		subjectList.add(subject);
+	}
+
+	public ArrayList<Subject> getSubjectList() {
+		return subjectList;
+	}
+
+	public void setSubjectList(ArrayList<Subject> subjectList) {
+		this.subjectList = subjectList;
+	}
+
+	public static String getSCHOOL_NAME() {
+		return SCHOOL_NAME;
+	}
+
+	public static void setSCHOOL_NAME(String sCHOOL_NAME) {
+		SCHOOL_NAME = sCHOOL_NAME;
+	}
+}
