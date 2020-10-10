b44c82ffb5982aa82b9adf9b58b3d32b6f36f626
kanggusdn <gusdnkang.DE@gmail.com>
2020-10-10 12:10:11 +0900

    집에서 복습하는 자바(챕터3)

---
+++ upgrade/src/upgrade/Chapter03_OperationEx4.java
@@ -0,0 +1,13 @@
+package upgrade;
+
+public class Chapter03_OperationEx4 {
+	public static void main(String[] args) {
+		int fatherAge = 45;
+		int motherAge = 47;
+		
+		char ch;
+		ch = (fatherAge > motherAge) ? 'T' : 'F';
+		
+		System.out.println(ch);
+	}
+}
---
+++ upgrade/src/upgrade/Chapter03_OperationEx5.java
@@ -0,0 +1,16 @@
+package upgrade;
+
+public class Chapter03_OperationEx5 {
+	public static void main(String[] args) {
+		int num = 0B00000101;
+		
+		System.out.println(num << 2);
+		System.out.println(num >> 2);
+		System.out.println(num >>> 2);
+		
+		System.out.println(num);
+		
+		num = num << 2;
+		System.out.println(num);
+	}
+}
