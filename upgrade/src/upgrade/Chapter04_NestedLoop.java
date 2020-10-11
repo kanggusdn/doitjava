229f623670f96f025eca816973b93f2ae2194837
kanggusdn <gusdnkang.DE@gmail.com>
2020-10-11 11:40:57 +0900

    집에서 복습하는 자바(챕터4)

---
+++ upgrade/src/upgrade/Chapter04_NestedLoop.java
@@ -0,0 +1,15 @@
+package upgrade;
+
+public class Chapter04_NestedLoop {
+	public static void main(String[] args) {
+		int dan;
+		int times;
+
+		for (dan = 2; dan <= 9; dan++) {
+			for (times = 1; times <= 9; times++) {
+				System.out.println(dan + "X" + times + "=" + dan * times);
+			}
+			System.out.println();
+		}
+	}
+}
